1cf42ae26337156fe00e053d0bf5568a3c1a53a5
hadiadjd <celestialgeek21@gmail.com>
2024-01-08 00:31:18 +0100

    change works card

--- task_2_portfolio/lib/works.js
+++ task_2_portfolio/lib/works.js
@@ -7,31 +7,23 @@ const works = [
   {
     id: 1,
     title: "10 Melon",
-    description: "10 Melon is a movie website designed with Next.js & Tailwind CSS, offering a diverse range of films. Providing detailed descriptions, trailers, and recommendations, it's the ultimate destination for movie enthusiasts.",
+    description: "10 Melon is a movie website with a diverse range of films & details for movie enthusiasts.",
     image: melon.src,
     path: "https://10melon.netlify.app/",
     code: "https://github.com/liliumorion/movie-project-alfrak-betikh"
   },
   {
     id: 2,
-    title: "madlibs",
-    description: "Madlibs is an entertaining word game that's suitable for all ages, where players fill in the blank with various types of words without knowing the context of the story, which often results in funny or nonsensical combinations.",
-    image: madlibs.src,
-    path: "https://liliumorion.github.io/madlibz/",
-    code: "https://github.com/liliumorion/madlibz"
-  },
-  {
-    id: 3,
     title: "AmaZD",
-    description: "AmaZD, crafted with Next.js & Tailwind CSS, is an user-friendly e-commerce platform that offers a range of products across different categories like fashion & electronics using a third-party API, ensuring an exceptional shopping journey.",
+    description: "AmaZD is a user-friendly e-commerce platform that ensures an exceptional shopping journey.",
     image: amazd.src,
     path: "https://e-commerce-project-amazon.vercel.app/",
     code: "https://github.com/liliumorion/e-commerce-project-amazon"
   },
   {
-    id: 4,
+    id: 3,
     title: "Unify",
-    description: "Unify is a platform that promotes charitable actions where users can exchange their daily items, ensuring community connections while making a positive social impact and encouraging sustainability.",
+    description: "Unify is a platform that promotes charitable actions & aimes to leave a positive social impact.",
     image: unify.src,
     path: "https://unify-team-a.vercel.app/",
     code: "https://github.com/liliumorion/Unify-team-a"
--- task_2_portfolio/src/components/Works.jsx
+++ task_2_portfolio/src/components/Works.jsx
@@ -5,45 +5,25 @@ const Works = () => {
   return (
     <section
       id="works"
-      className="pt-16 min-h-screen flex flex-col gap-10"
+      className="min-h-screen flex flex-col gap-10"
     >
-      <h2 className="text-4xl md:text-6xl xl:leading-[80px] tracking-[-2px] font-bold  text-center pl-6">
-        My Projects
+      <div>
+      <h2 className="text-4xl md:text-6xl font-bold py-2">
+        Works
       </h2>
+      </div>
       <div className="flex flex-col md:flex-row-reverse justify-center items-center gap-8">
-        {works.map((project) => (
-          <div key={project.id}
-            className="w-[300px] h-96 flex justify-center items-center bg-opacity-20 backdrop-filter backdrop-blur-md group rounded-xl shadow-2xl overflow-hidden cursor-pointer"
-          >
-            <div
-              className="relative w-full h-full bg-cover bg-no-repeat flex flex-col flex-1 justify-center items-center gap-4"
-              style={{
-                backgroundImage:`linear-gradient(rgba(31, 41, 55, 1), rgba(153, 26, 26, 0.7)), url(${project.image})`,
-              }}
-            >
-              <h2 className="font-black tracking-wider text-white text-4xl">
-                {project.title}
-              </h2>
-              <div className="flex flex-col gap-2 absolute p-8 text-xl font-light w-full h-full bg-red-800 text-white justify-center items-center translate-y-72 opacity-0 transition-all duration-700 ease-in-out group-hover:translate-y-0 group-hover:opacity-100">
-              <p className="text-lg">
-                {project.description}
-              </p>
-              <div className="flex items-center gap-2">
-              <button className="hover:bg-white hover:text-red-800 text-black border-2 border-white bg-transparent rounded-full p-1 w-24 text-base" >
-                <Link href={project.path} target="_blank">
-                  Preview
-                </Link>
-              </button>
-              <button className="hover:bg-white hover:text-red-800 text-black border-2 border-white bg-transparent rounded-full p-1 w-24 text-base" >
-                <Link href={project.code} target="_blank">
-                  Code
-                </Link>
-              </button>
-              </div>
-              </div>
-            </div>
-          </div>
-        ))}
+      {works.map((work) => (<div key={work.id} className="card w-[320px] h-[480px] glass">
+  <figure><img src={work.image} alt={work.title}/></figure>
+  <div className="card-body">
+    <h2 className="card-title">{work.title}</h2>
+    <p>{work.description}</p>
+    <div className="card-actions justify-end">
+      <Link className="btn btn-outline w-16" href={work.path} target="_blank">Demo</Link>
+      <Link className="btn btn-outline w-16" href={work.code} target="_blank">Code</Link>
+    </div>
+  </div>
+</div>))}
       </div>
     </section>
   );
